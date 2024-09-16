eebfb95c27407e6a8e83ab53af25f948428e10de
Yomkid <121500942+YomkidsBlog@users.noreply.github.com>
2024-09-16 18:11:24 +0100

    Update newNav3.php

--- app/Views/include/newNav3.php
+++ app/Views/include/newNav3.php
@@ -1,4 +1,3 @@
-<nav class="navbar navbar-expand-lg px-3">
+<nav class="navbar navbar-expand-lg px-3 text-center">
     <a class="navbar-brand" href="/">Learn<span style="color: #007bff;">X</span>a</a>
-    
 </nav>
