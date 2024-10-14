d311921c3cb1836004a6da1ec9216f8cbf516d34
Yomkid <121500942+YomkidsBlog@users.noreply.github.com>
2024-10-14 17:13:36 +0100

    Update CategoryModel.php

--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -15,8 +15,31 @@ class CategoryModel extends Model
     protected $allowedFields    = [
         'category_name',
         'category_description',
-        'category_image'
+        'category_image',
+        'slug'
     ];
+
+    public function getAllCategories()
+    {
+        return $this->findAll(); // This will return all categories
+    }
+
+
+public function getCoursesByCategory($categoryId)
+{
+    return $this->db->table('courses')
+        ->distinct() // Ensure unique results
+        ->select('courses.course_id, courses.course_title, courses.course_image, courses.price, courses.rating, courses.rating_count, courses.slug, instructors.first_name as instructor_name')
+        ->join('course_topics', 'course_topics.course_id = courses.course_id')
+        ->join('topic_categories', 'topic_categories.topic_id = course_topics.topic_id') // Join with topic_categories
+        ->join('instructors', 'instructors.instructor_id = courses.instructor_id', 'left') // Assuming there's an instructors table
+        ->where('topic_categories.category_id', $categoryId) // Filter on topic_categories
+        ->get()->getResult();
+}
+
+
+
+
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
