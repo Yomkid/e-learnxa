ff8a15be2c5a10792d2fe8832bae2d1f65fc190a
Yomkid <121500942+YomkidsBlog@users.noreply.github.com>
2024-10-31 13:27:19 +0100

    Update course_description.php

--- app/Views/course_description.php
+++ app/Views/course_description.php
@@ -233,7 +233,7 @@ s0.parentNode.insertBefore(s1,s0);
     <div class="course-heading hidden d-md-block">
         <h2 class="coursepage-title"><?= $title ?></h2>
         <div class="course-rating">
-            <span class="rating"><?= number_format($course->rating, 1) ?></span>
+            <span class="rating"><?= number_format($rating, 1) ?></span>
             <span class="rating-stars" style="color: #ffc107;">
                 <?php for ($i = 0; $i < floor($rating); $i++) : ?>
                     <i class="fa fa-star"></i>
