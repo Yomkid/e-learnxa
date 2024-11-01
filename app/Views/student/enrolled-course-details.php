4ecd96ccccff0772dd8d809d14aff617607b9638
Yomkid <121500942+YomkidsBlog@users.noreply.github.com>
2024-11-01 16:59:24 +0100

    Update enrolled-course-details.php

--- app/Views/student/enrolled-course-details.php
+++ app/Views/student/enrolled-course-details.php
@@ -81,7 +81,7 @@
                                 </div> -->
                                 <div class="progress mb-4">
                                     <div class="progress-bar" role="progressbar" style="width: <?= number_format($overallProgress, 1); ?>%;" aria-valuenow="<?= number_format($overallProgress, 1); ?>"
-                                        aria-valuemin="0" aria-valuemax="100"><?= number_format(number_format($overallProgress, 1), 1); ?>%</div>
+                                        aria-valuemin="0" aria-valuemax="100"><?= number_format($overallProgress, 1); ?>%</div>
                                 </div>
                             </div>
                            
@@ -146,8 +146,8 @@
                                     Course Progress
                                 </div>
                                 <div class="progress mb-4">
-                                    <div class="progress-bar" role="progressbar" style="width: <?= $overallProgress; ?>%;" aria-valuenow="<?= $overallProgress; ?>"
-                                        aria-valuemin="0" aria-valuemax="100"><?= $overallProgress; ?>%</div>
+                                    <div class="progress-bar" role="progressbar" style="width: <?= number_format($overallProgress, 1); ?>%;" aria-valuenow="<?= number_format($overallProgress, 1); ?>"
+                                        aria-valuemin="0" aria-valuemax="100"><?= number_format($overallProgress, 1); ?>%</div>
                                 </div>
                             </div>
                         </div>
