7bc50e52f3807aa6e1ef3ead6e21036276a50c77
Yomkid <121500942+YomkidsBlog@users.noreply.github.com>
2024-12-23 18:02:53 +0100

    Update Email copy.php

--- app/Config/Email copy.php
+++ app/Config/Email copy.php
@@ -6,7 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = 'elearnxa@gmail.com';
+    // public string $fromEmail  = 'elearnxa@gmail.com';
+    public string $fromEmail  = 'notifications@learnxa.com';
     public string $fromName   = 'LearnXa';
     // public string $recipients = [];
 
@@ -41,23 +42,26 @@ class Email extends BaseConfig
     /**
      * SMTP Server Hostname
      */
-    public string $SMTPHost = 'smtp.gmail.com';
+    // public string $SMTPHost = 'smtp.gmail.com';
+    public string $SMTPHost = 'mail.learnxa.com';
 
     /**
      * SMTP Username
      */
-    public string $SMTPUser = 'elearnxa@gmail.com';
+    // public string $SMTPUser = 'elearnxa@gmail.com';
+    public string $SMTPUser = 'notifications@learnxa.com';
 
     /**
      * SMTP Password
      */
-    public string $SMTPPass = 'wlrorxycddjqkuvh';
+    // public string $SMTPPass = 'wlrorxycddjqkuvh';
+    public string $SMTPPass = 'notification@learnxa';
     // wlro rxyc ddjq kuvh
 
     /**
      * SMTP Port
      */
-    public int $SMTPPort = 587;
+    public int $SMTPPort = 465;
 
     /**
      * SMTP Timeout (in seconds)
@@ -76,7 +80,7 @@ class Email extends BaseConfig
      *             to the server. 'ssl' means implicit SSL. Connection on port
      *             465 should set this to ''.
      */
-    public string $SMTPCrypto = 'tls';
+    public string $SMTPCrypto = 'ssl';
 
     /**
      * Enable word-wrap
